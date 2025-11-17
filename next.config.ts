e7a71d9a836755ffc4af33bcd0aa66916c0692c7
oswell-oxy <oswellokine1999@gmail.com>
2025-11-17 16:11:24 +0000

    Fixing nock issues part 3

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["sharp", "bcrypt"],
+    serverComponentsExternalPackages: ['sharp'],
+  },
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), 'nock'];
+    return config;
   },
 };
-
 export default nextConfig;
